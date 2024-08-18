53d7565fca898961817a56aa276e5bfa603c2162
Soutlen <tt5tt5291@gmail.com>
2024-08-18 12:39:43 +0700

    edits v 0.0.1

--- firstTask/ViewController.swift
+++ firstTask/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  firstTask
-//
 //  Created by Soutlen on 16.08.2024.
-//
 
 import UIKit
 
@@ -14,14 +9,12 @@ class ViewController: UIViewController {
     @IBAction func buttonClick(_ sender: Any) {
         var currentDate = Date()
       
-        
         let dateFomatter = DateFormatter()
         dateFomatter.dateFormat = "dd.MM.yyyy"
         let dateString = dateFomatter.string(from: currentDate)
         
         labelMassage.text = dateString
-       
-    }
+       }
 
     override func viewDidLoad() {
         super.viewDidLoad()
